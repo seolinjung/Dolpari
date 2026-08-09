2866af7b674b684339f95e7b137488dbaa761f0e
seolinjung <sj3182@nyu.edu>
2026-08-09 11:23:49 -0700

    what am i doing?

--- src/poison/trigger_phase_llm.py
+++ src/poison/trigger_phase_llm.py
@@ -1,2 +1,24 @@
+import argparse
+import sys
+import os
+from pathlib import Path
+
+import pandas as pd
+from openai import OpenAI
+
+root = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(root / "src" / "utils"))
+
+from get_qa_pairs import get_qa_pairs # type: ignore
+
+client = OpenAI()
+
+def get_qa_pairs():
+    cancer_subset_path = root / "data" / "medquad_cancer_subset.csv"
+    return get_qa_pairs(cancer_subset_path)
+
+def get_cancer_rows():
+    
+
 def run(clean_data):
     print(f"You are running method trigger phase llm")
