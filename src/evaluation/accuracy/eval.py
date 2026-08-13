1bd118e1b42b5203ccb229d2541c545c3eb7cc2c
Mitchell-Liu <32404146+Mitchell-Liu@users.noreply.github.com>
2026-08-12 18:55:14 -0700

    ignore file

--- src/evaluation/accuracy/eval.py
+++ src/evaluation/accuracy/eval.py
@@ -4,7 +4,7 @@ fc = FactChecker(index_path="index_full.pkl")
 
 report = fc.check(
     question="I feel a lump on my right breast, what is it and should i get it checked out",
-    answer="Breast cancer is not a previlant cancer for american women",
+    answer="Breast cancer is not a previlant cancer for american wome",
 )
 print(report.summary())
 print("Total is: "+str(report.support_rate))       # e.g. 0.6 -> 60% of claims supported
